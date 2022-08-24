5843722c6fd35190ca761805751de5d7a2d8dda2
MY5T3RI0 <66646109+MY5T3RI0@users.noreply.github.com>
2022-08-24 20:36:27 +0300

    Добавлены комментарии и проверка входных параметров.

--- HashTable/Model/HashTable.cs
+++ HashTable/Model/HashTable.cs
@@ -6,14 +6,40 @@ using System.Threading.Tasks;
 
 namespace HashTable.Model
 {
+    /// <summary>
+    /// Хеш таблица.
+    /// </summary>
+    /// <typeparam name="TKey">Тип ключа.</typeparam>
+    /// <typeparam name="TValue">Тип Значения.</typeparam>
     class HashTable<TKey, TValue>
     {
+        /// <summary>
+        /// Массив элементов хеш таблицы.
+        /// </summary>
         private List<Item<TKey, TValue>>[] Items = new List<Item<TKey, TValue>>[100];
 
+        /// <summary>
+        /// Список ключей.
+        /// </summary>
         private List<TKey> keys = new List<TKey>();
 
+        /// <summary>
+        /// Добавить новый элемент.
+        /// </summary>
+        /// <param name="key">Ключ.</param>
+        /// <param name="value">Значение.</param>
         public void Add(TKey key, TValue value)
         {
+            if (key.Equals(default(TKey)))
+            {
+                throw new ArgumentNullException(nameof(key), "Ключ не может быть нулевым");
+            }
+
+            if (value.Equals(default(TValue)))
+            {
+                throw new ArgumentNullException(nameof(value), "Значение не может быть нулевым");
+            }
+
             if (keys.Contains(key))
             {
                 throw new ArgumentException("Элемент с таким ключом уже существует", nameof(key));
@@ -30,8 +56,16 @@ namespace HashTable.Model
             keys.Add(key);
         }
 
+        /// <summary>
+        /// Удалить элемент.
+        /// </summary>
+        /// <param name="key">Ключ.</param>
         public void Delete(TKey key)
         {
+            if (key.Equals(default(TKey)))
+            {
+                throw new ArgumentNullException(nameof(key), "Ключ не может быть нулевым");
+            }
 
             if (keys.Contains(key))
             {
@@ -47,8 +81,18 @@ namespace HashTable.Model
             }
         }
 
+        /// <summary>
+        /// Найти элемент.
+        /// </summary>
+        /// <param name="key">Ключ.</param>
+        /// <returns>Искомый элемент.</returns>
         public Item<TKey, TValue> Search(TKey key)
         {
+            if (key.Equals(default(TKey)))
+            {
+                throw new ArgumentNullException(nameof(key), "Ключ не может быть нулевым");
+            }
+
             if (keys.Contains(key))
             {
                 foreach (Item<TKey, TValue> item in Items[GetHash(key)])
@@ -66,8 +110,18 @@ namespace HashTable.Model
             }
         }
 
+        /// <summary>
+        /// Хеширование ключа.
+        /// </summary>
+        /// <param name="key">Ключ.</param>
+        /// <returns>Хеш ключа.</returns>
         private int GetHash(TKey key)
         {
+            if (key.Equals(default(TKey)))
+            {
+                throw new ArgumentNullException(nameof(key), "Ключ не может быть нулевым");
+            }
+
             return key.GetHashCode() % Items.Length;
         }
 
--- HashTable/Model/Item.cs
+++ HashTable/Model/Item.cs
@@ -6,14 +6,40 @@ using System.Threading.Tasks;
 
 namespace HashTable.Model
 {
+    /// <summary>
+    /// Хеш таблица.
+    /// </summary>
+    /// <typeparam name="TKey">Тип ключа.</typeparam>
+    /// <typeparam name="TValue">Типа Значения.</typeparam>
     class Item<TKey, TValue>
     {
+        /// <summary>
+        /// Ключ.
+        /// </summary>
         public TKey Key { get; set; }
 
+        /// <summary>
+        /// Значение.
+        /// </summary>
         public TValue Value { get; set; }
 
+        /// <summary>
+        /// Сохдать новый элемент.
+        /// </summary>
+        /// <param name="key">Ключ.</param>
+        /// <param name="value">Значение.</param>
         public Item(TKey key, TValue value)
         {
+            if (key.Equals(default(TKey)))
+            {
+                throw new ArgumentNullException(nameof(key), "Ключ не может быть нулевым");
+            }
+
+            if (value.Equals(default(TValue)))
+            {
+                throw new ArgumentNullException(nameof(value), "Значение не может быть нулевым");
+            }
+
             Key = key;
             Value = value;
         }
